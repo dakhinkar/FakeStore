0f485b90813adf20f2871b340f7d9222ce404b3d
dakhinkar <pranitdakhinkar145@gmail.com>
2022-11-22 20:27:45 +0530

    heroku backend api added

--- src/apis/fakeStoreApi.js
+++ src/apis/fakeStoreApi.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export default axios.create({
     // baseURL: "https://fakestoreapi.com/"
-    baseURL:"http://localhost:8000/api"
+    // baseURL:"http://localhost:8000/api"
+    baseURL: "https://shop142.herokuapp.com/api"
 })
