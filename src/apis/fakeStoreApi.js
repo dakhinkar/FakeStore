9cc3f42f4e16711d9ccb10af03e8f047464cb7fc
dakhinkar <pranitdakhinkar145@gmail.com>
2022-12-06 13:26:05 +0530

    api change

--- src/apis/fakeStoreApi.js
+++ src/apis/fakeStoreApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://fakestoreapi.com/"
+    // baseURL: "https://fakestoreapi.com/"
     // baseURL:"http://localhost:5000/api"
-    // baseURL: "https://shop142.herokuapp.com/api"
+    baseURL: "https://shop142.herokuapp.com/api"
 })
